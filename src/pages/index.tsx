10d3f0ba0c18e80d01f19201b7f7fc6169a79a72
Augusto Dias <gutodiasux@gmail.com>
2023-04-20 09:31:55 -0300

    feat: add LoginPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
+import { Button, Grid } from '@chakra-ui/react'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>App works!</h1>
-    </main>
+    <>
+      <Head>
+        <title>Coupon Website</title>
+      </Head>
+      <Grid
+        as='main'
+        height={'100vh'}
+        alignItems={'center'}
+        justifyContent={'center'}
+      >
+        <Button
+          as={Link}
+          href={'/login'}
+          minWidth={'150px'}
+          maxWidth={'500px'}
+        >
+          Login
+        </Button>
+      </Grid>
+    </>
   )
 }
---
+++ src/pages/login.tsx
@@ -0,0 +1,45 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Grid,
+  Input,
+  VStack
+} from '@chakra-ui/react';
+import Head from 'next/head';
+
+export default function LoginPage() {
+  return (
+    <>
+      <Head>
+        <title>Coupon Website</title>
+      </Head>
+      <Grid
+        as='main'
+        height={'100vh'}
+        alignItems={'center'}
+        justifyContent={'center'}
+      >
+        <VStack
+          as='form'
+          gap={{ lg: '2' }}
+        >
+          <FormControl>
+            <FormLabel>Email</FormLabel>
+            <Input type='text' />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Password</FormLabel>
+            <Input type='password' />
+          </FormControl>
+          <Button
+            type={'submit'}
+            width={'100%'}
+          >
+            Entrar
+          </Button>
+        </VStack>
+      </Grid>
+    </>
+  )
+}
