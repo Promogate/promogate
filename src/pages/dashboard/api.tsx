ebb05dbcc191e03fc8cca56bf2ed815bc48f176f
Augusto Dias <gutodiasux@gmail.com>
2023-04-21 19:10:39 -0300

    feat: add api dashboard page

---
+++ src/pages/dashboard/api.tsx
@@ -0,0 +1,19 @@
+import { DashboardLayout } from '@/presentation/components'
+import { Heading } from '@chakra-ui/react'
+import Head from 'next/head'
+import { Fragment } from 'react'
+
+export default function Dashboard() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <DashboardLayout>
+        <Heading>
+          API works!
+        </Heading>
+      </DashboardLayout>
+    </Fragment>
+  )
+}
