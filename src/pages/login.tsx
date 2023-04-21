53150c8bcd686fb5c5cc20fbb01359380b23008f
Augusto Dias <gutodiasux@gmail.com>
2023-04-21 12:42:04 -0300

    feat: add handlers for login feature

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,14 +1,50 @@
+import { api } from '@/config';
+import { LoginData, RequestError } from '@/domain/models';
 import {
   Button,
   FormControl,
   FormLabel,
   Grid,
   Input,
-  VStack
+  VStack,
+  useToast
 } from '@chakra-ui/react';
+import { AxiosError } from 'axios';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { setCookie } from 'nookies';
+import React, { useState } from 'react';
 
 export default function LoginPage() {
+  const toast = useToast();
+  const router = useRouter();
+
+  const [loginData, setLoginData] = useState<LoginData>({
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    setLoginData({...loginData, [e.currentTarget.name]: e.currentTarget.value});
+  };
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    api.post<{ token: string }>('/users/signin', loginData).then((success) => {
+      setCookie(null, 'couponwebsite.access_token', success.data.token);
+      setLoginData({
+        email: '',
+        password: ''
+      });
+      router.push('/dashboard')
+    }).catch((error: AxiosError<RequestError>) => {
+      toast({
+        status: 'error',
+        position: 'top-right',
+        description: error.response?.data.message
+      })
+    })
+  };
 
   return (
     <>
@@ -27,15 +63,26 @@ export default function LoginPage() {
         >
           <FormControl>
             <FormLabel>Email</FormLabel>
-            <Input type='text' />
+            <Input 
+              type='text'
+              name='email'
+              value={loginData.email}  
+              onChange={handleChange}
+            />
           </FormControl>
           <FormControl>
             <FormLabel>Password</FormLabel>
-            <Input type='password' />
+            <Input 
+              type='password'
+              name='password'
+              value={loginData.password}
+              onChange={handleChange}
+            />
           </FormControl>
           <Button
             type={'submit'}
             width={'100%'}
+            onClick={handleLogin}
           >
             Entrar
           </Button>
