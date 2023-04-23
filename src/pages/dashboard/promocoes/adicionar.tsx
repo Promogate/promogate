5b97d5afa3254341709f3bfc86ee692e062b669c
Augusto Dias <gutodiasux@gmail.com>
2023-04-23 00:21:36 -0300

    feat: add create offer form

--- src/pages/dashboard/promocoes/adicionar.tsx
+++ src/pages/dashboard/promocoes/adicionar.tsx
@@ -1,17 +1,85 @@
+import { api } from '@/config'
 import { DashboardLayout } from '@/presentation/components'
 import {
+  Box,
+  Button,
   Flex,
+  FormControl,
+  FormLabel,
   Heading,
   IconButton,
-  useDisclosure
+  Input,
+  useToast
 } from '@chakra-ui/react'
 import Head from 'next/head'
 import Link from 'next/link'
-import { Fragment } from 'react'
+import { parseCookies } from 'nookies'
+import React, { Fragment, useState } from 'react'
 import { TfiAngleLeft } from 'react-icons/tfi'
+import { useMutation } from 'react-query'
+
+type OfferData = {
+  image: string,
+  title: string,
+  price: string,
+  oldPrice: string,
+  destinationLink: string,
+  storeImage: string,
+  expirationDate: string
+}
 
 export default function AddOffersPage() {
-  const { onOpen, onClose, isOpen } = useDisclosure()
+  const cookies = parseCookies();
+  const toast = useToast();
+  const [offerData, setOfferData] = useState<OfferData>({
+    image: '',
+    title: '',
+    price: '',
+    oldPrice: '',
+    destinationLink: '',
+    storeImage: '',
+    expirationDate: ''
+  })
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    setOfferData({ ...offerData, [e.currentTarget.name]: e.currentTarget.value })
+  }
+
+  const createOffer = useMutation(async () => {
+    await api.post('/resources/offer/create', offerData, {
+      headers: {
+        'X-API-KEY': 'BRSEW0QC5N4VCAGS5572H85JV7W2',
+        Authorization: `Bearer ${cookies['couponwebsite.access_token']}`
+      }
+    })
+  }, {
+    onSuccess: () => {
+      toast({
+        status: 'success',
+        description: 'Oferta adicionada com sucesso!'
+      }),
+        setOfferData({
+          image: '',
+          title: '',
+          price: '',
+          oldPrice: '',
+          destinationLink: '',
+          storeImage: '',
+          expirationDate: ''
+        })
+    },
+    onError: (e: any) => {
+      toast({
+        status: 'error',
+        description: e.message
+      })
+    }
+  })
+
+  const handleCreateOffer = async () => {
+    await createOffer.mutateAsync()
+  }
 
   return (
     <Fragment>
@@ -25,8 +93,8 @@ export default function AddOffersPage() {
           alignItems={'center'}
         >
           <IconButton
-          as={Link}
-          href='/dashboard/promocoes'
+            as={Link}
+            href='/dashboard/promocoes'
             aria-label='create'
             variant={'outline'}
           >
@@ -40,6 +108,100 @@ export default function AddOffersPage() {
             Adicionar oferta
           </Heading>
         </Flex>
+        <Box
+          padding={{ lg: '2rem 0' }}
+        >
+          <Box
+            as={'form'}
+            display={'grid'}
+            gridTemplateColumns={{ lg: '1fr 1fr' }}
+            gap={{ lg: '1rem' }}
+            backgroundColor={'white'}
+            padding={{ lg: '1.5rem' }}
+            borderRadius={{ lg: '1rem' }}
+          >
+            <FormControl>
+              <FormLabel>Image da oferta</FormLabel>
+              <Input
+                type='text'
+                name='image'
+                value={offerData.image}
+                onChange={handleChange}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Título</FormLabel>
+              <Input
+                type='text'
+                name='title'
+                value={offerData.title}
+                onChange={handleChange}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Preço antigo</FormLabel>
+              <Input
+                type='text'
+                name='oldPrice'
+                value={offerData.oldPrice}
+                onChange={handleChange}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Preço atual</FormLabel>
+              <Input
+                type='text'
+                name='price'
+                value={offerData.price}
+                onChange={handleChange}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Link da oferta</FormLabel>
+              <Input
+                type='text'
+                name='destinationLink'
+                value={offerData.destinationLink}
+                onChange={handleChange}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Image da loja</FormLabel>
+              <Input
+                type='text'
+                name='storeImage'
+                value={offerData.storeImage}
+                onChange={handleChange}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Data de expiração</FormLabel>
+              <Input
+                type='datetime-local'
+                name='expirationDate'
+                value={offerData.expirationDate}
+                onChange={handleChange}
+              />
+            </FormControl>
+          </Box>
+          <Flex
+            justifyContent={'flex-end'}
+            padding={{ lg: '1rem 0' }}
+          >
+            <Button
+              size={'lg'}
+              backgroundColor={'black'}
+              _hover={{
+                backgroundColor: 'black'
+              }}
+              color={'white'}
+              onClick={handleCreateOffer}
+              isLoading={createOffer.isLoading}
+            >
+              Adicionar oferta
+            </Button>
+          </Flex>
+        </Box>
       </DashboardLayout>
     </Fragment>
   )
