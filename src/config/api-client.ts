0d2963693d475df69a8a59fb753f3f7bc2048086
Augusto Dias <gutodiasux@gmail.com>
2023-04-26 23:07:26 -0300

    feat: add API_KEY in axios instance

--- src/config/api-client.ts
+++ src/config/api-client.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
+import { API_KEY } from './variables';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: 'http://localhost:8080',
+  headers:{
+    'X-API-KEY': API_KEY
+  }
 })
