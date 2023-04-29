4d48ee4c48235bc1b6c4920edc3536be61480566
Augusto Dias <gutodiasux@gmail.com>
2023-04-29 12:29:25 -0300

    refact: change api url strategy

--- src/config/api-client.ts
+++ src/config/api-client.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { API_URL } from './variables';
 
+const setupAPIURL = () => {
+  return process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : API_URL
+}
+
 export const api = axios.create({
-  baseURL: (process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : API_URL),
+  baseURL: setupAPIURL()
 })
