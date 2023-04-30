a5896f7113ea9523b982b65cc0c3f3114b4d0b2e
Augusto Dias <gutodiasux@gmail.com>
2023-04-30 12:38:59 -0300

    refact: arrange api route on production

--- src/config/internal-api-client.ts
+++ src/config/internal-api-client.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { API_URL } from './variables';
 
 export const internalApiClient = axios.create({
-  baseURL: (process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : API_URL)
+  baseURL: (process.env.NODE_ENV === 'development' ? '/api': 'https://api.promogate.app/')
 })
