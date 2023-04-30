7e377bebfbd042c4462889d2f78140974f82b3ef
Augusto Dias <gutodiasux@gmail.com>
2023-04-30 15:40:27 -0300

    feat: check if user is already logged in login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,21 +7,28 @@ import {
   FormControl,
   FormLabel,
   Grid,
+  Heading,
   Input,
   Text,
   useToast
 } from '@chakra-ui/react';
 import { AxiosError } from 'axios';
+import { GetServerSideProps } from 'next';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { setCookie } from 'nookies';
+import { parseCookies, setCookie } from 'nookies';
 import React, { useState } from 'react';
+import { BsArrowRightShort } from 'react-icons/bs';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function LoginPage() {
+type LoginPageProps = {
+  isLogged: string | undefined
+}
+
+export default function LoginPage({ isLogged }: LoginPageProps) {
   const toast = useToast();
   const router = useRouter();
 
@@ -67,73 +74,109 @@ export default function LoginPage() {
         <Box
           margin={'0 auto'}
         >
-          <Flex
-            as='form'
-            minWidth={{ xl: '400px' }}
-            flexDirection={'column'}
-            gap={{ xl: '1.175rem' }}
-          >
-            <FormControl>
-              <FormLabel
-                fontFamily={inter.style.fontFamily}
-              >
-                Email
-              </FormLabel>
-              <Input
-                type='text'
-                name='email'
-                value={loginData.email}
-                onChange={handleChange}
-                fontFamily={inter.style.fontFamily}
-                size={'lg'}
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel
-                fontFamily={inter.style.fontFamily}
-              >
-                Senha
-              </FormLabel>
-              <Input
-                type='password'
-                name='password'
-                value={loginData.password}
-                onChange={handleChange}
-                size={'lg'}
-              />
-            </FormControl>
-            <Button
-              type={'submit'}
-              width={'100%'}
-              onClick={handleLogin}
-              size={'lg'}
-              backgroundColor={'#571770'}
-              color={'white'}
-              transition={'375ms ease-out'}
-              _hover={{
-                backgroundColor: '#7e2b9e'
-              }}
-              fontFamily={inter.style.fontFamily}
-            >
-              Entrar
-            </Button>
-          </Flex>
-          <Flex
-            margin={{ xl: '1rem 0 0 0' }}
-            fontFamily={inter.style.fontFamily}
-            fontSize={{ xl: '0.825rem' }}
-            gap={'2px'}
-            alignItems={'center'}
-          >
-            <Text>Ainda não é cadastrado?</Text>
-            <Text
-              as={Link} href={'/cadastrar-se'}
-              fontWeight={'semibold'}
-              color={'#571770'}
-            >
-              Cadastre-se
-            </Text>
-          </Flex>
+          {
+            isLogged ? (
+              <>
+                <Heading
+                  as='h2'
+                  fontFamily={inter.style.fontFamily}
+                  fontSize={{ xl: '1.5rem' }}
+                  color={'gray.600'}
+                >
+                  Você já está logado!
+                </Heading>
+                <Flex
+                  fontFamily={inter.style.fontFamily}
+                  fontSize={{ xl: '0.825rem' }}
+                  gap={'2px'}
+                  alignItems={'center'}
+                  margin={{ xl: '0.5rem 0 0 0' }}
+                >
+                  <Text>Vá direto para o</Text>
+                  <Text
+                    as={Link} href={'/dashboard'}
+                    fontWeight={'semibold'}
+                    color={'#571770'}
+                    display={'flex'}
+                    alignItems={'center'}
+                  >
+                    Dashboard
+                    <BsArrowRightShort />
+                  </Text>
+                </Flex>
+              </>
+            ) : (
+              <>
+                <Flex
+                  as='form'
+                  minWidth={{ xl: '400px' }}
+                  flexDirection={'column'}
+                  gap={{ xl: '1.175rem' }}
+                >
+                  <FormControl>
+                    <FormLabel
+                      fontFamily={inter.style.fontFamily}
+                    >
+                      Email
+                    </FormLabel>
+                    <Input
+                      type='text'
+                      name='email'
+                      value={loginData.email}
+                      onChange={handleChange}
+                      fontFamily={inter.style.fontFamily}
+                      size={'lg'}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <FormLabel
+                      fontFamily={inter.style.fontFamily}
+                    >
+                      Senha
+                    </FormLabel>
+                    <Input
+                      type='password'
+                      name='password'
+                      value={loginData.password}
+                      onChange={handleChange}
+                      size={'lg'}
+                    />
+                  </FormControl>
+                  <Button
+                    type={'submit'}
+                    width={'100%'}
+                    onClick={handleLogin}
+                    size={'lg'}
+                    backgroundColor={'#571770'}
+                    color={'white'}
+                    transition={'375ms ease-out'}
+                    _hover={{
+                      backgroundColor: '#7e2b9e'
+                    }}
+                    fontFamily={inter.style.fontFamily}
+                  >
+                    Entrar
+                  </Button>
+                </Flex>
+                <Flex
+                  margin={{ xl: '1rem 0 0 0' }}
+                  fontFamily={inter.style.fontFamily}
+                  fontSize={{ xl: '0.825rem' }}
+                  gap={'2px'}
+                  alignItems={'center'}
+                >
+                  <Text>Ainda não é cadastrado?</Text>
+                  <Text
+                    as={Link} href={'/cadastrar-se'}
+                    fontWeight={'semibold'}
+                    color={'#571770'}
+                  >
+                    Cadastre-se
+                  </Text>
+                </Flex>
+              </>
+            )
+          }
         </Box>
         <Box
           backgroundColor={'#F5F4F7'}
@@ -148,4 +191,14 @@ export default function LoginPage() {
       </Grid>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  return {
+    props: {
+      isLogged: cookies['promogate.token']
+    }
+  }
 }
