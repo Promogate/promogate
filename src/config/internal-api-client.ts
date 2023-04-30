5ab48d5eef9d640b59836440dd9e80688412d089
Augusto Dias <gutodiasux@gmail.com>
2023-04-30 00:15:51 -0300

    refact: add rewrites

--- src/config/internal-api-client.ts
+++ src/config/internal-api-client.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
+import { API_URL } from './variables';
 
 export const internalApiClient = axios.create({
-  baseURL: (process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '/api')
+  baseURL: (process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : API_URL)
 })
