72d1b31fb69ae8b5ab6c4a5120bc9c0ff9487b20
Augusto Dias <gutodiasux@gmail.com>
2023-04-30 15:57:22 -0300

    feat: check if user is already logged in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,32 @@
 import { MainMenu } from '@/presentation/components'
+import { GetServerSideProps } from 'next'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+import { parseCookies } from 'nookies'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+type HomeProps = {
+  isLogged: string | undefined
+}
+
+export default function Home({ isLogged }: HomeProps) {
   return (
     <>
       <Head>
         <title>Coupon Website</title>
       </Head>
-      <MainMenu />
+      <MainMenu isLogged={isLogged} />
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  return {
+    props: {
+      isLogged: cookies['promogate.token']
+    }
+  }
 }
--- src/presentation/components/main-menu.tsx
+++ src/presentation/components/main-menu.tsx
@@ -1,11 +1,16 @@
-import { Box, Flex, Grid } from '@chakra-ui/react';
+import { Box, Flex, Grid, Text } from '@chakra-ui/react';
 import { Inter } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
+import { BsArrowRightShort } from 'react-icons/bs';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export function MainMenu() {
+type MainMenuProps = {
+  isLogged: string | undefined
+}
+
+export function MainMenu({ isLogged }: MainMenuProps) {
   return (
     <Grid
       gridTemplateColumns={'auto 1fr auto'}
@@ -47,27 +52,53 @@ export function MainMenu() {
         fontSize={'0.8rem'}
         color={'gray.500'}
       >
-        <Box
-          as={Link}
-          href={'/login'}
-          fontFamily={inter.style.fontFamily}
-          _hover={{
-            textDecoration: 'underline'
-          }}
-        >
-          Entrar
-        </Box>
-        <Box
-          as={Link}
-          href={'/cadastrar-se'}
-          fontFamily={inter.style.fontFamily}
-          _hover={{
-            textDecoration: 'underline'
-          }}
-        >
-          Cadastrar-se
-        </Box>
-      </Flex>
-    </Grid>
+        {
+          isLogged ? (
+            <Flex
+              fontFamily={inter.style.fontFamily}
+              fontSize={{ xl: '0.825rem' }}
+              gap={'2px'}
+              alignItems={'center'}
+              margin={{ xl: '0.5rem 0 0 0' }}
+            >
+              <Text>Você está logado. Ir para o </Text>
+              <Text
+                as={Link} href={'/dashboard'}
+                fontWeight={'semibold'}
+                color={'#571770'}
+                display={'flex'}
+                alignItems={'center'}
+              >
+                Dashboard
+                <BsArrowRightShort />
+              </Text>
+            </Flex>
+          ) : (
+            <>
+              <Box
+                as={Link}
+                href={'/login'}
+                fontFamily={inter.style.fontFamily}
+                _hover={{
+                  textDecoration: 'underline'
+                }}
+              >
+                Entrar
+              </Box>
+              <Box
+                as={Link}
+                href={'/cadastrar-se'}
+                fontFamily={inter.style.fontFamily}
+                _hover={{
+                  textDecoration: 'underline'
+                }}
+              >
+                Cadastrar-se
+              </Box>
+            </>
+          )
+        }
+      </Flex >
+    </Grid >
   )
 }
