105e5cae1952a79293dab53835325d404e377002
Augusto Dias <gutodiasux@gmail.com>
2023-05-02 19:20:21 -0300

    refact: arrange AuthContext

--- src/application/contexts/auth-context.tsx
+++ src/application/contexts/auth-context.tsx
@@ -1,5 +1,9 @@
+import { userState } from '@/application/states/atoms';
 import { api } from '@/config';
+import { useRouter } from 'next/router';
+import { setCookie } from 'nookies';
 import { ReactNode, createContext } from 'react';
+import { useRecoilState } from 'recoil';
 
 type SigninInput = {
   email: string;
@@ -7,24 +11,37 @@ type SigninInput = {
 }
 
 type SigninOuput = {
-  token: string
+  token: string;
+  user: string;
+  profile: string;
+}
+
+type User = {
+  user: string;
+  profile: string;
 }
 
 interface AuthContextProps {
-  signIn(input: SigninInput): Promise<SigninOuput>
+  signIn(input: SigninInput): Promise<SigninOuput>;
+  user: User
 }
 
 export const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
 
 export function AuthContextProvider ({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  const [user, setUser] = useRecoilState(userState)
   
   async function signIn(input: SigninInput): Promise<SigninOuput> {
-    const { data } = await api.post('/users/sigin', input);
+    const { data } = await api.post<SigninOuput>('/signin', input);
+    setUser({ user: data.user, profile: data.profile })
+    setCookie(null, 'promogate.token', data.token);
+    router.push('/dashboard');
     return data
   }
 
   return (
-    <AuthContext.Provider value={{ signIn }}>
+    <AuthContext.Provider value={{ signIn, user }}>
       {children}
     </AuthContext.Provider>
   )
