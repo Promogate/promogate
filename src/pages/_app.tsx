49ca9db075fef1ad802462a41f9d4fe5a35be824
Augusto Dias <gutodiasux@gmail.com>
2023-05-02 19:16:06 -0300

    feat: implement RecoilRoot

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,23 @@
-import { PromogateContextProvider } from '@/application/contexts';
+import { AuthContextProvider, PromogateContextProvider } from '@/application/contexts';
 import { queryClient } from '@/config';
 import '@/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <PromogateContextProvider>
-        <ChakraProvider>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </PromogateContextProvider>
+      <RecoilRoot>
+        <AuthContextProvider>
+          <PromogateContextProvider>
+            <ChakraProvider>
+              <Component {...pageProps} />
+            </ChakraProvider>
+          </PromogateContextProvider>
+        </AuthContextProvider>
+      </RecoilRoot>
     </QueryClientProvider>
   )
 }
