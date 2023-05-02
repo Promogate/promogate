faeb7500dc66b090c05bf883723eeabfab09a762
Augusto Dias <gutodiasux@gmail.com>
2023-05-02 19:21:26 -0300

    refact(fix): api axios instance

--- src/config/api-client.ts
+++ src/config/api-client.ts
@@ -4,7 +4,7 @@ import { parseCookies } from 'nookies';
 const cookies = parseCookies();
 
 export const api = axios.create({
-  baseURL: (process.env.NODE_ENV === 'development' ? 'http://localhost:8080/': '/api'),
+  baseURL: (process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '/api'),
   headers: {
     Authorization: `Bearer ${cookies['promogate.token']}`
   }
