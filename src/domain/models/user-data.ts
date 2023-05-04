17a3e2dfca64362cf7f44e4ee5f9750ac16cd47a
Augusto Dias <gutodiasux@gmail.com>
2023-05-04 10:54:11 -0300

    refact: change UserData model to a complete structure

--- src/domain/models/user-data.ts
+++ src/domain/models/user-data.ts
@@ -1,4 +1,13 @@
 export type UserData = {
-  userId: string,
-  userApiKey: string
+	id: string,
+	email: string,
+	name: string,
+	created_at: string,
+	user_profile: {
+		id: string,
+		store_image: string,
+		store_name: string,
+		role: string,
+		user_id: string
+	}
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
-import { getOffersWithAnalytics } from '@/application/remote'
+import { PromogateContext } from '@/application/contexts'
 import { api } from '@/config'
+import { UserData } from '@/domain/models'
 import { DashboardLayout } from '@/presentation/components'
 import { withSSRAuth } from '@/utils'
 import {
@@ -22,211 +23,228 @@ import {
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import { parseCookies } from 'nookies'
-import { Fragment } from 'react'
+import { Fragment, useContext } from 'react'
 import { useQuery } from 'react-query'
 
 const inter = Inter({ subsets: ['latin'] })
 
+type DashboardProps = {
+  user: UserData
+}
+
 /* eslint-disable @next/next/no-img-element */
-export default function Dashboard() {
-  const cookies = parseCookies();
+export default function Dashboard({ user }: DashboardProps) {
+  const { fetchOffers} = useContext(PromogateContext);
 
-  const { data, isLoading } = useQuery(['analytics', cookies['promogate.token']], async () => {
-    const { data } = await api.get<{ clicks: number }>('/dashboard/analytics/clicks')
-
-    return data
-  }, {
-    staleTime: 1000 * 5,
-    cacheTime: 1000 * 5
-  })
-
-  const offers = useQuery(['offers', 'dashboard'], getOffersWithAnalytics, {
+  const { data, isLoading, isError } = useQuery(['offers', user.id], fetchOffers, {
     staleTime: 1000 * 60 * 5,
-    cacheTime: 1000 * 60 * 5
+    cacheTime: 1000 * 60 * 5,
   })
 
+  const offers: any = Array<any>
+  
   return (
     <Fragment>
       <Head>
         <title>Dashboard</title>
       </Head>
       <DashboardLayout>
-        <Heading
-          as={'h2'}
-          fontSize={{ xl: '2xl' }}
-          fontFamily={inter.style.fontFamily}
-          color={'gray.600'}
-        >
-          Dashboard
-        </Heading>
-        <Grid
-          gridTemplateColumns={{ xl: 'repeat(3, 1fr)' }}
-          margin={{ xl: '1rem 0' }}
-          gap={{ xl: '1rem' }}
-        >
-          <GridItem
-            backgroundColor={'white'}
-            boxShadow={'sm'}
-            borderRadius={{ xl: '0.5rem' }}
-            padding={{ xl: '1rem' }}
-          >
-            <Heading
-              as={'span'}
-              fontSize={{ xl: 'md' }}
-              fontFamily={inter.style.fontFamily}
-              fontWeight={'normal'}
-              color={'gray.500'}
-            >
-              Total de cliques
-            </Heading>
-            <Heading
-              as={'h3'}
-              fontSize={{ xl: '3xl' }}
-              color={'gray.600'}
-            >
-              <Skeleton isLoaded={!isLoading}>
-                {data?.clicks}
-              </Skeleton>
-            </Heading>
-          </GridItem>
-          <GridItem
-            backgroundColor={'white'}
-            boxShadow={'sm'}
-            borderRadius={{ xl: '0.5rem' }}
-            padding={{ xl: '1rem' }}
-          >
-            <Heading
-              as='h3'
-              fontFamily={inter.style.fontFamily}
-              fontSize={{ xl: '0.825rem' }}
-              color={'gray.400'}
-            >
-              Em Breve
-            </Heading>
-          </GridItem>
-          <GridItem
-            backgroundColor={'white'}
-            boxShadow={'sm'}
-            borderRadius={{ xl: '0.5rem' }}
-            padding={{ xl: '1rem' }}
-          >
-            <Heading
-              as='h3'
-              fontFamily={inter.style.fontFamily}
-              fontSize={{ xl: '0.825rem' }}
-              color={'gray.400'}
-            >
-              Em Breve
-            </Heading>
-          </GridItem>
-        </Grid>
-        <Box
-          padding={{ xl: '2rem 0' }}
-        >
-          <Heading
-            as={'h2'}
-            fontSize={{ xl: 'xl' }}
-            fontFamily={inter.style.fontFamily}
-            color={'gray.600'}
-            marginBottom={{ xl: '1rem' }}
-          >
-            Top 10 produtos mais clicados
-          </Heading>
-          <Box
-            backgroundColor={'white'}
-            padding={{ xl: '1rem 0' }}
-            borderRadius={{ xl: '0.5rem' }}
-          >
-            {
-              offers.isLoading ? (
-                <Spinner />
-              ) : (offers.data?.length === 0) ? (
-                <Box margin={'0 auto'} textAlign={'center'} alignItems={'center'}>
+        {
+          user.user_profile.id ? (
+            <Fragment>
+              <Heading
+                as={'h2'}
+                fontSize={{ xl: '2xl' }}
+                fontFamily={inter.style.fontFamily}
+                color={'gray.600'}
+              >
+                Dashboard
+              </Heading>
+              <Grid
+                gridTemplateColumns={{ xl: 'repeat(3, 1fr)' }}
+                margin={{ xl: '1rem 0' }}
+                gap={{ xl: '1rem' }}
+              >
+                <GridItem
+                  backgroundColor={'white'}
+                  boxShadow={'sm'}
+                  borderRadius={{ xl: '0.5rem' }}
+                  padding={{ xl: '1rem' }}
+                >
                   <Heading
-                    as={'h2'}
-                    fontSize={{ xl: 'lg' }}
-                    fontWeight={'normal'}
+                    as={'span'}
+                    fontSize={{ xl: 'md' }}
                     fontFamily={inter.style.fontFamily}
-                    color={'gray.300'}
+                    fontWeight={'normal'}
+                    color={'gray.500'}
                   >
-                    Você ainda não tem ofertas cadastradas
+                    Total de cliques
                   </Heading>
-                </Box>
-              ) : (
-                <Table size={'sm'}>
-                  <Thead>
-                    <Tr>
-                      <Th>Imagem do produto</Th>
-                      <Th>Título do produto</Th>
-                      <Th>Cliques</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    {offers.data?.map((offer) => {
-                      return (
-                        <Tr
-                          key={offer.id}
-                          backgroundColor={'white'}
+                  <Heading
+                    as={'h3'}
+                    fontSize={{ xl: '3xl' }}
+                    color={'gray.600'}
+                  >
+                    <Skeleton isLoaded={!isLoading}>
+                      {data?.clicks}
+                    </Skeleton>
+                  </Heading>
+                </GridItem>
+                <GridItem
+                  backgroundColor={'white'}
+                  boxShadow={'sm'}
+                  borderRadius={{ xl: '0.5rem' }}
+                  padding={{ xl: '1rem' }}
+                >
+                  <Heading
+                    as='h3'
+                    fontFamily={inter.style.fontFamily}
+                    fontSize={{ xl: '0.825rem' }}
+                    color={'gray.400'}
+                  >
+                    Em Breve
+                  </Heading>
+                </GridItem>
+                <GridItem
+                  backgroundColor={'white'}
+                  boxShadow={'sm'}
+                  borderRadius={{ xl: '0.5rem' }}
+                  padding={{ xl: '1rem' }}
+                >
+                  <Heading
+                    as='h3'
+                    fontFamily={inter.style.fontFamily}
+                    fontSize={{ xl: '0.825rem' }}
+                    color={'gray.400'}
+                  >
+                    Em Breve
+                  </Heading>
+                </GridItem>
+              </Grid>
+              <Box
+                padding={{ xl: '2rem 0' }}
+              >
+                <Heading
+                  as={'h2'}
+                  fontSize={{ xl: 'xl' }}
+                  fontFamily={inter.style.fontFamily}
+                  color={'gray.600'}
+                  marginBottom={{ xl: '1rem' }}
+                >
+                  Top 10 produtos mais clicados
+                </Heading>
+                <Box
+                  backgroundColor={'white'}
+                  padding={{ xl: '1rem 0' }}
+                  borderRadius={{ xl: '0.5rem' }}
+                >
+                  {
+                    offers.isLoading ? (
+                      <Spinner />
+                    ) : (offers.data?.length === 0) ? (
+                      <Box margin={'0 auto'} textAlign={'center'} alignItems={'center'}>
+                        <Heading
+                          as={'h2'}
+                          fontSize={{ xl: 'lg' }}
+                          fontWeight={'normal'}
+                          fontFamily={inter.style.fontFamily}
+                          color={'gray.300'}
                         >
-                          <Td>
-                            <Link
-                              href={`/dashboard/promocoes/${offer.id}`}
-                            >
-                              <Box
-                                position={'relative'}
-                                width={'64px'}
-                                height={'auto'}
+                          Você ainda não tem ofertas cadastradas
+                        </Heading>
+                      </Box>
+                    ) : (
+                      <Table size={'sm'}>
+                        <Thead>
+                          <Tr>
+                            <Th>Imagem do produto</Th>
+                            <Th>Título do produto</Th>
+                            <Th>Cliques</Th>
+                          </Tr>
+                        </Thead>
+                        <Tbody>
+                          {offers.data?.map((offer) => {
+                            return (
+                              <Tr
+                                key={offer.id}
+                                backgroundColor={'white'}
                               >
-                                <img
-                                  src={offer.image}
-                                  alt={offer.title}
-                                />
-                              </Box>
-                            </Link>
-                          </Td>
-                          <Td>
-                            <Link
-                              href={`/dashboard/promocoes/${offer.id}`}
-                            >
-                              <Text
-                                fontStyle={inter.style.fontStyle}
-                                fontWeight={'medium'}
-                                color={'gray.600'}
-                              >
-                                {offer.title}
-                              </Text>
-                            </Link>
-                          </Td>
-                          <Td>
-                            <Flex
-                              width={'100%'}
-                            >
-                              <Text
-                                fontStyle={inter.style.fontStyle}
-                                fontWeight={'medium'}
-                                color={'gray.600'}
-                              >
-                                {offer._count.offer_clicks}
-                              </Text>
-                            </Flex>
-                          </Td>
-                        </Tr>
-                      )
-                    })
-                    }
-                  </Tbody>
-                </Table>
-              )
-            }
-          </Box>
-        </Box>
+                                <Td>
+                                  <Link
+                                    href={`/dashboard/promocoes/${offer.id}`}
+                                  >
+                                    <Box
+                                      position={'relative'}
+                                      width={'64px'}
+                                      height={'auto'}
+                                    >
+                                      <img
+                                        src={offer.image}
+                                        alt={offer.title}
+                                      />
+                                    </Box>
+                                  </Link>
+                                </Td>
+                                <Td>
+                                  <Link
+                                    href={`/dashboard/promocoes/${offer.id}`}
+                                  >
+                                    <Text
+                                      fontStyle={inter.style.fontStyle}
+                                      fontWeight={'medium'}
+                                      color={'gray.600'}
+                                    >
+                                      {offer.title}
+                                    </Text>
+                                  </Link>
+                                </Td>
+                                <Td>
+                                  <Flex
+                                    width={'100%'}
+                                  >
+                                    <Text
+                                      fontStyle={inter.style.fontStyle}
+                                      fontWeight={'medium'}
+                                      color={'gray.600'}
+                                    >
+                                      {offer._count.offer_clicks}
+                                    </Text>
+                                  </Flex>
+                                </Td>
+                              </Tr>
+                            )
+                          })
+                          }
+                        </Tbody>
+                      </Table>
+                    )
+                  }
+                </Box>
+              </Box>
+            </Fragment>
+          ) : (
+            <Heading>
+              Precisa criar uma loja
+            </Heading>
+          )
+        }
       </DashboardLayout>
     </Fragment>
   )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  const { data } = await api.get('/users/me', {
+    headers: {
+      Authorization: `Bearer ${cookies['promogate.token']}`
+    }
+  })
+
   return {
-    props: {}
+    props: {
+      user: data
+    }
   }
 }) 
