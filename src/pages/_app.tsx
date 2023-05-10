f55ff553bb5bcc419787788aab7df6c2a6b4d27d
Augusto Dias <gutodiasux@gmail.com>
2023-05-10 17:13:07 -0300

    refact: quill css impl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '@/styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { QueryClientProvider } from 'react-query';
+import 'react-quill/dist/quill.snow.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
