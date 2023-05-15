27e64a45a029e6dd6e8502fed3363d8bee7de6b6
Augusto Dias <gutodiasux@gmail.com>
2023-05-15 13:51:11 -0300

    refact: remove from auth context the user state responsability

--- src/application/contexts/auth-context.tsx
+++ src/application/contexts/auth-context.tsx
@@ -1,9 +1,7 @@
-import { userState } from '@/application/atoms';
 import { api } from '@/config';
 import { useRouter } from 'next/router';
 import { setCookie } from 'nookies';
 import { ReactNode, createContext } from 'react';
-import { useRecoilState } from 'recoil';
 
 type SignInInput = {
   email: string;
@@ -41,8 +39,25 @@ type SignUpInput = {
 
 type SignUpOuput = {
   token: string;
-  user: string;
-  profile: string;
+  user: {
+    id: string,
+		name: string,
+		email: string,
+		created_at: string,
+		user_profile: {
+			id: string,
+			store_image: string,
+			store_name: string,
+			role: string,
+			social_media: ({
+        facebook: string | null,
+        instagram: string | null,
+        whatsapp: string | null,
+        telegram: string | null,
+        twitter: string | null
+      } | null)
+		}
+  }
 }
 
 interface AuthContextProps {
@@ -54,29 +69,9 @@ export const AuthContext = createContext<AuthContextProps>({} as AuthContextProp
 
 export function AuthContextProvider ({ children }: { children: ReactNode }) {
   const router = useRouter();
-  const [user, setUser] = useRecoilState(userState);
   
   async function signIn(input: SignInInput): Promise<SignInOuput> {
     const { data } = await api.post<SignInOuput>('/signin', input);
-    setUser({
-      id: data.user.id,
-      name: data.user.name,
-      email: data.user.email,
-      created_at: data.user.created_at,
-      user_profile: {
-        id: data.user.user_profile.id,
-        role: data.user.user_profile.role,
-        store_name: data.user.user_profile.store_name,
-        store_image: data.user.user_profile.store_image,
-        social_media: {
-          facebook: data.user.user_profile.social_media?.facebook ? data.user.user_profile.social_media?.facebook : '',
-          instagram: data.user.user_profile.social_media?.instagram ? data.user.user_profile.social_media?.instagram : '',
-          whatsapp: data.user.user_profile.social_media?.whatsapp ? data.user.user_profile.social_media?.whatsapp : '',
-          telegram: data.user.user_profile.social_media?.telegram ? data.user.user_profile.social_media?.telegram : '',
-          twitter: data.user.user_profile.social_media?.twitter ? data.user.user_profile.social_media?.twitter : '',
-        }
-      }
-    })
     setCookie(null, 'promogate.token', data.token);
     router.push('/dashboard');
     return data
