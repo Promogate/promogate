3d4d01c1d073429cb889e6c21ff4f73678300ae4
Augusto Dias <gutodiasux@gmail.com>
2023-05-15 13:51:53 -0300

    refact: remove recoil root from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,20 +5,17 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { QueryClientProvider } from 'react-query';
 import 'react-quill/dist/quill.snow.css';
-import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <AuthContextProvider>
-          <PromogateContextProvider>
-            <ChakraProvider>
-              <Component {...pageProps} />
-            </ChakraProvider>
-          </PromogateContextProvider>
-        </AuthContextProvider>
-      </QueryClientProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <PromogateContextProvider>
+          <ChakraProvider>
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </PromogateContextProvider>
+      </AuthContextProvider>
+    </QueryClientProvider>
   )
 }
