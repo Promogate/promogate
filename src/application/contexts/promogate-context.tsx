0d4057411cbce52ffc7a58102cfccd95c238073f
Augusto Dias <gutodiasux@gmail.com>
2023-05-15 13:52:19 -0300

    build: add user state responsability in promogate context

--- src/application/contexts/promogate-context.tsx
+++ src/application/contexts/promogate-context.tsx
@@ -3,8 +3,8 @@ import { DashboardData, OfferWithClicks, RequestError } from '@/domain/models';
 import { useToast } from '@chakra-ui/react';
 import { AxiosError } from 'axios';
 import { useRouter } from 'next/router';
-import { parseCookies } from 'nookies';
-import { ReactNode, createContext } from 'react';
+import { destroyCookie, parseCookies } from 'nookies';
+import { ReactNode, createContext, useEffect, useState } from 'react';
 
 type CreateProfileInput = {
   store_name: string;
@@ -31,13 +31,39 @@ export type FetchStoreOffersResponse = {
   }
 }
 
+type MeResponse = {
+  status: string,
+  user: User
+}
+
+type User = {
+  id: string,
+  name: string,
+  email: string,
+  created_at: string,
+  user_profile: {
+    id: string,
+    store_image: string,
+    store_name: string,
+    role: string,
+    social_media: ({
+      facebook: string | null,
+      instagram: string | null,
+      whatsapp: string | null,
+      telegram: string | null,
+      twitter: string | null
+    } | null)
+  }
+}
+
 interface PromogateContextProps {
   createUserProfile(input: CreateProfileInput): Promise<void>
   fetchDashboardData(profileId: string): Promise<DashboardData>;
   fetchStoreOffers(storeName: string): Promise<FetchStoreOffersResponse>
   fetchUserData(): Promise<any>;
   fetchStoreData(storeName: string): Promise<any>;
-  token: string
+  authorization: string;
+  user: User;
 }
 
 export const PromogateContext = createContext<PromogateContextProps>({} as PromogateContextProps);
@@ -47,6 +73,58 @@ export function PromogateContextProvider({ children }: { children: ReactNode }) 
   const toast = useToast();
   const cookies = parseCookies();
   const router = useRouter();
+  const [user, setUser] = useState<User>({
+    id: '',
+    name: '',
+    email: '',
+    created_at: '',
+    user_profile: {
+      id: '',
+      role: '',
+      store_name: '',
+      store_image: '',
+      social_media: {
+        facebook: '',
+        instagram: '',
+        whatsapp: '',
+        telegram: '',
+        twitter: '',
+      }
+    }
+  });
+
+  useEffect(() => {
+    try {
+      api.get<MeResponse>('/users/me', {
+        headers: {
+          Authorization: `Bearer ${cookies['promogate.token']}`
+        }
+      }).then((fullfiled) => {
+        const { data } = fullfiled
+        setUser({
+          id: data.user.id,
+          name: data.user.name,
+          email: data.user.email,
+          created_at: data.user.created_at,
+          user_profile: {
+            id: data.user.user_profile.id,
+            role: data.user.user_profile.role,
+            store_name: data.user.user_profile.store_name,
+            store_image: data.user.user_profile.store_image,
+            social_media: {
+              facebook: data.user.user_profile.social_media?.facebook ? data.user.user_profile.social_media?.facebook : '',
+              instagram: data.user.user_profile.social_media?.instagram ? data.user.user_profile.social_media?.instagram : '',
+              whatsapp: data.user.user_profile.social_media?.whatsapp ? data.user.user_profile.social_media?.whatsapp : '',
+              telegram: data.user.user_profile.social_media?.telegram ? data.user.user_profile.social_media?.telegram : '',
+              twitter: data.user.user_profile.social_media?.twitter ? data.user.user_profile.social_media?.twitter : '',
+            }
+          }
+        })
+      })
+    } catch {
+      destroyCookie(null, 'promogate.token');
+    }
+  })
 
   async function fetchDashboardData(profileId: string): Promise<DashboardData> {
     const { data } = await api.get<DashboardData>(`/analytics/profile/${profileId}`, {
@@ -109,7 +187,8 @@ export function PromogateContextProvider({ children }: { children: ReactNode }) 
       createUserProfile,
       fetchStoreOffers,
       fetchStoreData,
-      token: `Bearer ${cookies['promogate.token']}`
+      authorization: `Bearer ${cookies['promogate.token']}`,
+      user
     }}>
       {children}
     </PromogateContext.Provider>
