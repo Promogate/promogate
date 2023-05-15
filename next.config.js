d9453fc928e8b328057b1b2429ca9cbd02e659a6
Augusto Dias <gutodiasux@gmail.com>
2023-05-15 16:31:21 -0300

    hotfix(test): user API_URL to setup api client

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites () {
-    return [
-      {
-        source: '/api/external/:path*',
-        destination: 'https://api.promogate.app/:path*',
-      },
-    ]
-  },
   images: {
     domains: ['promogate.s3.sa-east-1.amazonaws.com']
   }
--- src/config/api-client.ts
+++ src/config/api-client.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { parseCookies } from 'nookies';
+import { API_URL } from './variables';
 
 const cookies = parseCookies();
 
 export const api = axios.create({
-  baseURL: (process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '/api/external'),
+  baseURL: (process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : API_URL),
   headers: {
     Authorization: `Bearer ${cookies['promogate.token']}`
   }
