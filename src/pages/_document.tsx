e01110156f1ab3aec4c87a47d411778e9ee235b1
Augusto Dias <gutodiasux@gmail.com>
2023-05-16 19:39:58 -0300

    refact: change favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9258286762385899"
           crossOrigin="anonymous"></script>
+          <link rel='icon' type='image/png' href='/favicon.png' />
       </Head>
       <body>
         <Main />
