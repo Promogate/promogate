37cfd32d4a8a02e7b857abb718e5489368d3dffe
Augusto Dias <gutodiasux@gmail.com>
2023-05-29 10:12:57 -0300

    refact: remove quotation marks from url

--- src/main/utils/parse-ambient-url.ts
+++ src/main/utils/parse-ambient-url.ts
@@ -1,5 +1,5 @@
 export const parseAmbientUrl = (value: string) => {
   const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://promogate.app/'
 
-  return `${url}${value.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[\s,]/g, '-')}`
+  return `${url}${value.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f,.'‘’"“”+]/g, '').replace(/[\s]/g, '-')}`
 }
