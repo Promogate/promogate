d5b63f87815aacdd25c8df26767e213742b591c2
Augusto Dias <gutodiasux@gmail.com>
2023-06-18 12:43:25 -0300

    feat: add Integrations page

---
+++ src/pages/dashboard/integracoes/index.tsx
@@ -0,0 +1,99 @@
+import { api } from '@/config';
+import { MeResponse } from '@/domain/models';
+import { DashboardLayout } from '@/presentation/components';
+import { withSSRAuth } from '@/utils';
+import { Box, Button, Flex, Grid, GridItem, Heading } from '@chakra-ui/react';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { parseCookies } from 'nookies';
+import { RiSettings3Line } from 'react-icons/ri';
+
+const INTEGRATION_LINKS = [
+  {
+    destination: '/dashboard/integracoes/socialsoul',
+    name: 'Configurações',
+    image: '/logo_social-soul.webp'
+  }
+]
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  const { data } = await api.get<MeResponse>('/users/me', {
+    headers: {
+      Authorization: `Bearer ${cookies['promogate.token']}`
+    }
+  })
+
+  return {
+    props: {
+      status: data.status,
+      user: data.user
+    }
+  }
+})
+
+export default function Integrations() {
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <title>Promogate - Integrações</title>
+      </Head>
+      <DashboardLayout>
+        <Flex
+          width={['100%']}
+          marginBottom={['2rem']}
+        >
+          <Heading
+            as={'h2'}
+            fontSize={['2rem']}
+            color={'gray.600'}
+          >
+            Integrações
+          </Heading>
+        </Flex>
+        <Grid
+          gridTemplateColumns={['1fr', '1fr', 'repeat(3, 1fr)', 'repeat(4, 1fr)']}
+        >
+          {
+            INTEGRATION_LINKS.map((item, i) => {
+              return (
+                <GridItem
+                  key={i}
+                  width={['100%']}
+                  backgroundColor={['white']}
+                  padding={['1rem']}
+                  borderRadius={['md']}
+                >
+                  <Box
+                    position={['relative']}
+                    width={'160px'}
+                    height={'56px'}
+                    margin={['0 auto']}
+                  >
+                    <Image
+                      src={item.image}
+                      alt='Logo SocialSoul'
+                      fill
+                    />
+                  </Box>
+                  <Button
+                    leftIcon={<RiSettings3Line />}
+                    width={['100%']}
+                    marginTop={['2rem']}
+                    onClick={() => router.push(item.destination)}
+                  >
+                    {item.name}
+                  </Button>
+                </GridItem>
+              )
+            })
+          }
+        </Grid>
+      </DashboardLayout>
+    </>
+  )
+}
