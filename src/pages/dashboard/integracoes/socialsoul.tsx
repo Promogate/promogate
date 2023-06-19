8cd99063e9d553feaae70cb71bf8d4160c8f3a5f
Augusto Dias <gutodiasux@gmail.com>
2023-06-19 16:24:14 -0300

    refact: add socialsoul integration form

--- src/pages/dashboard/integracoes/socialsoul.tsx
+++ src/pages/dashboard/integracoes/socialsoul.tsx
@@ -1,9 +1,15 @@
+import { PromogateContext } from '@/application/contexts';
 import { api } from '@/config';
 import { MeResponse } from '@/domain/models';
 import { DashboardLayout } from '@/presentation/components';
 import { withSSRAuth } from '@/utils';
+import { Box, Button, FormControl, FormLabel, Grid, Input, useToast } from '@chakra-ui/react';
+import { useMutation } from '@tanstack/react-query';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { parseCookies } from 'nookies';
+import { FormEvent, useContext, useState } from 'react';
+import { FiSend } from 'react-icons/fi';
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const cookies = parseCookies(ctx);
@@ -21,7 +27,81 @@ export const getServerSideProps = withSSRAuth(async (ctx) => {
   }
 })
 
+type IntegrationFormData = {
+  sourceId: string | undefined;
+  file: any | undefined;
+}
+
 export default function SocialSoulIntegrationPage({ status, user }: MeResponse) {
+  const [formData, setFormData] = useState<IntegrationFormData>({
+    sourceId: undefined,
+    file: undefined
+  })
+
+  const router = useRouter();
+
+  const toast = useToast();
+
+  const { authorization } = useContext(PromogateContext);
+
+  function handleFormData(event: FormEvent<HTMLInputElement>) {
+    event.preventDefault();
+    setFormData({ ...formData, [event.currentTarget.name]: event.currentTarget.value });
+  }
+
+  console.log(formData);
+
+  const mutation = useMutation({
+    mutationFn: async () => await handleUploadFormData(),
+    onError: (error: any) => {
+      toast({
+        status: 'error',
+        description: error.message
+      })
+    },
+    onSuccess: () => {
+      toast({
+        status: 'success',
+        description: 'Atualizado com sucesso;'
+      })
+      router.reload();
+    }
+  })
+
+  async function triggerMutation(): Promise<void> {
+    await mutation.mutateAsync();
+  };
+
+  async function handleUploadFormData() {
+    const fileData = new FormData()
+    fileData.append('file', formData.file)
+    try {
+      await api.post(`/upload/${user.user_profile.store_name}`, fileData, {
+        headers: {
+          Authorization: authorization
+        }
+      })
+      await api.put(`/users/profile/${user.user_profile.id}/update`, {
+        lomadeeSourceId: formData.sourceId
+      }, {
+        headers: {
+          Authorization: authorization
+        }
+      })
+    } catch (err: any) {
+      toast({
+        status: 'error',
+        description: 'Algo deu errado'
+      })
+    }
+  }
+
+  function handleFile(event: FormEvent<HTMLInputElement>) {
+    event.preventDefault();
+    if (event.currentTarget.files) {
+      setFormData({ ...formData, file: event.currentTarget.files[0] })
+    }
+  }
 
   if (!user.user_profile.lomadee_source_id || user.user_profile.lomadee_source_id === '') {
     return (
@@ -30,7 +110,49 @@ export default function SocialSoulIntegrationPage({ status, user }: MeResponse) 
           <title>Promogate - Integração SocialSoul</title>
         </Head>
         <DashboardLayout>
-
+          <Grid
+            height={['100vh']}
+            placeItems={['center']}
+          >
+            <Box
+              display={['grid']}
+              gap={['1rem']}
+              as='form'
+              width={['400px']}
+              backgroundColor={['white']}
+              padding={['2rem']}
+              borderRadius={['lg']}
+              border={['1px']}
+              borderColor={['gray.200']}
+            >
+              <FormControl>
+                <FormLabel htmlFor='sourceId'>Insira seu sourceId</FormLabel>
+                <Input
+                  name='sourceId'
+                  type='text'
+                  onChange={(e) => handleFormData(e)}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor='sourceId'>Arquivo de verificação HTML</FormLabel>
+                <Input
+                  name='file'
+                  type='file'
+                  accept='.html'
+                  onChange={(e) => handleFile(e)}
+                  border={'none'}
+                />
+              </FormControl>
+              <Button
+                leftIcon={<FiSend />}
+                colorScheme={'green'}
+                onClick={triggerMutation}
+                isLoading={mutation.isLoading}
+              >
+                Enviar informações
+              </Button>
+            </Box>
+          </Grid>
         </DashboardLayout>
       </>
     )
