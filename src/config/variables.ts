9308a7b628b0b4f560b655256ef8c1677591a364
Augusto Dias <gutodiasux@gmail.com>
2023-06-21 14:08:06 -0300

    feat: add social soul environment variables

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -1,5 +1,9 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+const SOCIALSOUL_API_URL = process.env.NEXT_PUBLIC_SOCIALSOUL_API_URL as string;
+const SOCIALSOUL_APP_ID = process.env.NEXT_PUBLIC_SOCIALSOUL_APP_ID as string;
 
 export {
-  API_URL
+  API_URL,
+  SOCIALSOUL_API_URL,
+  SOCIALSOUL_APP_ID
 };
