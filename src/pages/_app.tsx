62475f822bd1822cdc5e3a9259d7e18e59bf0d91
Augusto Dias <gutodiasux@gmail.com>
2023-06-26 12:11:48 -0300

    chore: add recoil and recoil persist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,20 +7,23 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import 'react-quill/dist/quill.snow.css';
+import { RecoilRoot } from 'recoil';
 import '../styles/slider.css';
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthContextProvider>
-        <PromogateContextProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <PromogateContextProvider>
             <ChakraProvider>
               <Component {...pageProps} />
             </ChakraProvider>
-        </PromogateContextProvider>
-      </AuthContextProvider>
-    </QueryClientProvider>
+          </PromogateContextProvider>
+        </AuthContextProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   )
 }
